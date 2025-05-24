e5ce9425ddca21b3fa0b43795faad1c365145f7f
Paul M. Rodriguez <prodriguez@grammatech.com>
2025-05-24 16:00:31 -0500

    Print ratios as floats by default

--- scripts/math.lisp
+++ scripts/math.lisp
@@ -5,24 +5,49 @@
    :lg :lb :ln :sq :hypot)
   (:local-nicknames
    (:c :with-c-syntax)
+   (:cli :clingon)
    (:fp :floating-point-contractions))
   (:documentation "Do math with C-style syntax but a Lisp numeric tower")
   (:export :main))
 (in-package :kiln/scripts/math)
 
+(def +options+
+  (list
+   (cli:make-option
+    :flag
+    :description "Print ratios"
+    :short-name #\R
+    :long-name "print-ratios"
+    :key :ratiop)))
+
+(def +command+
+  (cli:make-command
+   :name "kiln-math"
+   :options +options+))
+
 (defun main (args)
-  (let* ((string (string-join args ""))
+  (let* ((opts (cli:parse-command-line +command+ args))
+         (args (cli:command-arguments opts))
+         (string (string-join args ""))
          ;; Print double floats without suffix.
          (*read-default-float-format* 'double-float)
          (*readtable*
            (find-readtable 'c:with-c-syntax-readtable))
          (*package*
-           (find-package :kiln/scripts/math)))
-    (format t "~a~%"
-            (handler-bind ((warning #'muffle-warning))
-              (eval
-               (read-from-string
-                (string+ "#2{" string "}#")))))))
+           (find-package :kiln/scripts/math))
+         (result
+           (handler-bind ((warning #'muffle-warning))
+             (eval
+              (read-from-string
+               ;; 2 means to split C operators inside Lisp symbols.
+               (string+ "#2{" string "}#")))))
+         (result
+           (if (typep result 'ratio)
+               (if (cli:getopt opts :ratiop)
+                   result
+                   (coerce result *read-default-float-format*))
+               result)))
+    (format t "~a~%" result)))
 
 (defsubst log1p (x) (fp:log1+ x))
 (defsubst log1m (x) (fp:log1- x))

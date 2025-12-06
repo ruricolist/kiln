a9ba8c75b9303ec959406f7a3eaa486bc2d0647d
Paul M. Rodriguez <pmr@ruricolist.com>
2025-12-06 16:34:52 -0600

    Improve kiln REPL
    Don't print readably, and re-enable the debugger (disabled by default).

--- scripts/repl.lisp
+++ scripts/repl.lisp
@@ -10,17 +10,22 @@
   (format *error-output* "Write ~s or send EOF to quit" :quit)
   (with-standard-io-syntax
     (let ((*print-circle* t)
-          (uiop:*lisp-interaction* t))
-      (loop (format *error-output* "~%> ")
-            (let ((input (read *standard-input* nil :quit)))
-              (if (eql input :quit)
-                  (progn
-                    (terpri *error-output*)
-                    (return))
-                  (let ((values (multiple-value-list (eval input))))
-                    (dolist (value values)
-                      (format t "~s" value)
-                      (finish-output)))))))))
+          (*print-readably* nil)
+          (uiop:*lisp-interaction* t)
+          ;; Re-enable the disabled debugger.
+          #+sbcl
+          (sb-ext:*invoke-debugger-hook* nil))
+      (handler-bind ((error #'invoke-debugger))
+        (loop (format *error-output* "~%> ")
+              (let ((input (read *standard-input* nil :quit)))
+                (if (eql input :quit)
+                    (progn
+                      (terpri *error-output*)
+                      (return))
+                    (let ((values (multiple-value-list (eval input))))
+                      (dolist (value values)
+                        (format t "~s" value)
+                        (finish-output))))))))))
 
 (defun main (args)
   (declare (ignore args))
